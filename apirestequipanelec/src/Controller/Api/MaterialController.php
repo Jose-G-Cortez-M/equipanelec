da927ded2aebc1830320786eee97a01dd725fa3e
jose-g-cortez-m <josecortez1991_@hotmail.com>
2021-07-06 02:43:17 -0500

    Agregar metodos get y post a materiales

---
+++ apirestequipanelec/src/Controller/Api/MaterialController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Controller\Api;
+
+use App\Entity\Material;
+use App\Form\Type\MaterialFormType;
+use App\Repository\MaterialRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+
+class MaterialController extends AbstractFOSRestController
+{
+    /**
+     * @Rest\Get(path="/material")
+     * @Rest\View(serializerGroups={"material"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function getAction(
+        MaterialRepository $materialRepository
+    ) {
+        return $materialRepository->findAll();
+    }
+
+    /**
+     * @Rest\Post(path="/material")
+     * @Rest\View(serializerGroups={"material"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function postAction(
+        EntityManagerInterface $em,
+        Request $request
+    ) {
+        $material = new Material();
+        $material->setNombre('tuercas');
+        $em->persist($material);
+        $em->flush();
+        return $material;
+    }
+}
