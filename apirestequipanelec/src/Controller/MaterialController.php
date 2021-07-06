5de2792a0241367651b225ce505e860d6b9d45a2
jose-g-cortez-m <josecortez1991_@hotmail.com>
2021-07-06 01:57:04 -0500

    Material Fixes #15

---
+++ apirestequipanelec/src/Controller/MaterialController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Material;
+use App\Repository\MaterialRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MaterialController extends AbstractController
+{
+    /**
+     * @Route("/material/list", name="material_list")
+     */
+    public function list(Request $request, MaterialRepository $materialRepository){
+        $material = $materialRepository->findAll();
+        $materialAsArray = [];
+        foreach ($material as $material) {
+            $materialAsArray[] = [
+                'id' => $material->getId(),
+                'nombre' => $material->getNombre(),
+                'imagen' => $material->getImagen()
+            ];
+        };
+        $response = new JsonResponse();
+        $response->setData([
+            'success' => true,
+            'data' => $materialAsArray
+        ]);
+        return $response;
+    }   
+    /**
+     * @Route("/material/crear", name="crear_material")
+     */
+    public function createMaterial(Request $request, EntityManagerInterface $em){
+        $material = new Material();
+        $response = new JsonResponse();
+        $nombre = $request->get('nombre', null);
+        if (empty($title)) {
+            $response->setData([
+                'success' => false,
+                'error' => 'Title cannot be empty',
+                'data' => null
+            ]);
+            return $response;
+        }
+        $material->setNombre($nombre);
+        $em->persist($material);
+        $em->flush();
+        $response->setData([
+            'success' => true,
+            'data' => [
+                [
+                    'id' => $material->getId(),
+                    'nombre' => $material->getNombre()
+                ]
+            ]
+        ]);
+        return $response;
+        
+    }
+}
---
+++ apirestequipanelec/src/Entity/Material.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MaterialRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MaterialRepository::class)
+ */
+class Material
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=512, nullable=true)
+     */
+    private $imagen;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(?string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+}
